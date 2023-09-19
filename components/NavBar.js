ee3cd5cfcc5247f8b8506e027bba402ba07e9970
gahyeon <pgh2031@inpix.com>
2023-09-19 13:28:12 +0900

    #1.4 CSS Modules

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,13 +1,17 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   console.log(router);
+
   return (
     <nav>
-      <Link href="/" className="hello" style={{ color: router.pathname == "/" ? "red" : "" }}>Home</Link>
-      <Link href="/about" style={{ color: router.pathname == "/about" ? "red" : "" }}>About</Link>
+      <Link href="/" className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</Link>
+      <Link href="/about" className={
+        [styles.link, router.pathname === "/about" ? styles.active : ""].join(" ")
+      }>About</Link>
     </nav>
   )
 }
