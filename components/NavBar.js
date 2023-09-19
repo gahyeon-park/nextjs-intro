e33fc36a51fa89bebdc55fcba6fcde35fcc126a0
gahyeon <pgh2031@inpix.com>
2023-09-19 13:10:20 +0900

    #1.3 Routing

---
+++ components/NavBar.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar() {
+  const router = useRouter();
+  console.log(router);
+  return (
+    <nav>
+      <Link href="/" className="hello" style={{ color: router.pathname == "/" ? "red" : "" }}>Home</Link>
+      <Link href="/about" style={{ color: router.pathname == "/about" ? "red" : "" }}>About</Link>
+    </nav>
+  )
+}
